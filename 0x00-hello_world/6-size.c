a982e621ee99f776202f7da75308abcc99ed9cd6
Gitsvh <ashermutale82@gmail.com>
2022-12-08 05:44:06 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main -print out sizes of data types in c
+ * code by matrix decoded
+ * return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char: %lu byte(S)\n", (unsigned long) size of (a));
+	printf("size of a int: %lu byte(S)\n", (unsigned long) size of (b));
+	printf("size of a long int: %lu byte(S)\n", (unsigned long) size of (c));
+	printf("size of a long long int: %lu byte(S)\n", (unsigned long) size of (d));
+	printf("size of a float: %lu byte(S)\n" (unsigned long) size of (f));
+	return (0)
+}
