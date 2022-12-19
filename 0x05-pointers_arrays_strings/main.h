ce3df0508d32bda2442350daf5ec555c3e882fbc
Gitsvh <ashermutale82@gmail.com>
2022-12-19 02:13:29 -0800

    task2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-
-
-
-
+#ifndef MAIN_H
+#define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,4 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
+#endif
