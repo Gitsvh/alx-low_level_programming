58773200846403f90ef714367336fdad743c0d65
Gitsvh <ashermutale82@gmail.com>
2023-01-31 23:43:05 -0800

    loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds loop i linked list
+ * @head: pointer to head
+ * Return: listint_t
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	slow = head;
+	fast = head;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
